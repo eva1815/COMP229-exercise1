6e62ccedfc8a3f69bbf03aeaaff080716e23abf2
eva1815 <evafan1815@gmail.com>
2022-10-16 14:31:34 -0400

    Authentication completed

--- app/routes/auth.route.server.js
+++ app/routes/auth.route.server.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
-import { DisplayLoginPage, DisplayRegisterPage } from "../controllers/auth.controller.server.js";
+import { DisplayLoginPage, DisplayRegisterPage, ProcessLoginPage, ProcessLogoutPage, ProcessRegisterPage } from "../controllers/auth.controller.server.js";
 
 const router = Router();
 
 //Display login Router
 router.get('/login', DisplayLoginPage);
+//process login page
+router.post('/login', ProcessLoginPage);
 
 //Display Register Router
 router.get('/register', DisplayRegisterPage);
+//process registration page
+router.post('/register', ProcessRegisterPage);
+
+//process logout page
+router.get('/logout', ProcessLogoutPage);
+
+
 
 export default router;
