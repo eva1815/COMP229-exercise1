cadd971a9e0307d3976dfe625550490db7e2ce49
eva1815 <evafan1815@gmail.com>
2022-10-16 10:52:29 -0400

    Mongo Altlas Setup Completed

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI = "mongodb://127.0.0.1/media";
+//export const MongoURI = "mongodb://127.0.0.1/media";
+export const MongoURI = "mongodb+srv://eva:Canada170613@cluster0.qlp6cqc.mongodb.net/media?retryWrites=true&w=majority";
